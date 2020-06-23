609ca55ec167d43f2c6b1ccd7907234bae926787
wyattscarpenter <wyattscarpenter@gmail.com>
2020-06-23 04:16:14 -0700

    basically, spelling

--- print.h
+++ print.h
@@ -6,13 +6,14 @@ Simply #include "print.h" and call print on whatever you want, up to 16 argument
 Fully standard under C11 and later.
 Can print the same types of data as printf.
 (In fact, the implementation of the macro uses printf, so this is sort of just a safer and more convenient way of calling printf.)
-(However, we can't inline character literals, because those are by standard promoted to ints on creation. Sorry, I don't make the rules. Use inline string literals instead.)
+(However, we can't print inline character literals, because those are by standard promoted to ints on creation, or something.
+ Sorry, I don't make the rules. Use inline string literals instead.)
 Arguments are evalutated only once in the expanded macro, so stuff like print(i++) is safe.
 */
 
 #include <stdio.h>
 
-//we count on promotion rules I don't understand
+//We rely on promotion rules I don't really understand.
 //TODO: could replace printf. But is it already optimized out by the compiler?
 void _print_int(long long int i){printf("%lld", i);}
 void _print_uint(unsigned long long int u){printf("%llu", u);}
@@ -36,8 +37,8 @@ void _print_unknown(unsigned long long int x){printf("%llX", x);} //compiler iss
   default: _print_unknown \
 ) (unit)
 
-//Now, to make the function variadic
-// Make a FOREACH macro for up to 16 arguments
+//Now, to make the function variadic.
+//Make a FOR_EACH macro for up to 16 arguments:
 #define _FE_0(WHAT)
 #define _FE_1(WHAT, X) WHAT(X); 
 #define _FE_2(WHAT, X, ...) WHAT(X); _FE_1(WHAT, __VA_ARGS__)
@@ -56,9 +57,8 @@ void _print_unknown(unsigned long long int x){printf("%llX", x);} //compiler iss
 #define _FE_15(WHAT, X, ...) WHAT(X); _FE_14(WHAT, __VA_ARGS__)
 #define _FE_16(WHAT, X, ...) WHAT(X); _FE_15(WHAT, __VA_ARGS__)
 
-
-//This is magic and I don't understand it
+//This is magic and I understand it.
 //credit to https://stackoverflow.com/a/11994395 and the answers that answer is based on
 #define _GET_MACRO(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, NAME, ...) NAME 
 #define _FOR_EACH(action,...) _GET_MACRO(_0,__VA_ARGS__, _FE_16, _FE_15, _FE_14, _FE_13, _FE_12, _FE_11, _FE_10, _FE_9, _FE_8, _FE_7, _FE_6, _FE_5, _FE_4, _FE_3, _FE_2, _FE_1, _FE_0)(action,__VA_ARGS__)
-#define print(...) _FOR_EACH(_print_unit,__VA_ARGS__)
+#define print(...) _FOR_EACH(_print_unit, __VA_ARGS__)
