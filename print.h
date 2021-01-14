847a911ef588d5be8f29cbc63d840466efbb79a3
wyattscarpenter <wyattscarpenter@gmail.com>
2021-01-14 16:55:30 +0000

    better description

--- print.h
+++ print.h
@@ -1,15 +1,18 @@
 /*print.h - Finally, a print function!
+  A compile-time-type-safe, variadic, free-form print() function, fully standard-compliant, C11 and later.
   Simply #include "print.h" and call print on whatever you want, 1 to 32 arguments.
   32 is arbitrary, and the number of arguments print can take can be expanded straightforwardly.
   You will receive a compile-time warning if you exceed the limit, don't worry.
   Written in and for pure C.
-  Fully standard-compliant, C11 and later.
   Can print the same types of data as printf, and a few more! And you can add your own!
   In fact, the implementation of the macro uses printf, so this is sort of just a safer and more convenient way of calling printf.
+  Can print arrays of any size, unless they are the same size as a pointer.
   However, we can't print inline character literals, because those are by standard promoted to ints on creation, or something.
   Sorry, I don't make the rules. Use inline string literals instead.
   Arguments are evaluated only once at run time in the expanded macro, so stuff like print(i++) is safe.
   Only a handful of symbols are introduced into the namespace, most of them printing functions.
+  This "header" file currently includes implementations, as I'm not quite sure how I want to architect it more properly.
+  This header file currently #includes stdio.h, so don't bother double-including stdio.h.
   I release this code into the public domain under CC0.
 */
 
